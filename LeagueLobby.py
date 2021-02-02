117fc715b1a31532bc1b710a8106549f8487b8f6
XHXIAIEIN <45864744+XHXIAIEIN@users.noreply.github.com>
2021-02-02 16:39:19 +0800

    Update LeagueLobby.py

--- LeagueLobby.py
+++ LeagueLobby.py
@@ -16,16 +16,6 @@ from lcu_driver import Connector
 #-------------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------------
-# 参考资料
-#-------------------------------------------------------------------------------
-#  - LCU API 速查手册
-#    https://lcu.vivide.re/#operation--lol-lobby-v2-lobby-get
-#
-#  - 训练模式数据格式
-#    https://riot-api-libraries.readthedocs.io/en/latest/lcu.html
-#-------------------------------------------------------------------------------
-
-#-------------------------------------------------------------------------------
 # 接下来的计划 TODO 
 # 1. 可视化GUI
 #-------------------------------------------------------------------------------
@@ -35,37 +25,37 @@ connector = Connector()
 #-------------------------------------------------------------------------------
 # 获取召唤师信息
 #-------------------------------------------------------------------------------
-async def getSummonerInfo(connection):
+async def get_summoner_info(connection):
   summoner = await connection.request('get', '/lol-summoner/v1/current-summoner')
   if summoner.status == 200:
     data = await summoner.json()
-    print(f'[获取信息] 召唤师：{data["displayName"]} (等级：{data["summonerLevel"]}）')
+    print(f"[获取信息] 召唤师：{data['displayName']} (等级：{data['summonerLevel']}）")
   else:
     print('账号信息获取失败')
 
 #-------------------------------------------------------------------------------
 # 创建训练模式 5V5自定义房间
 #-------------------------------------------------------------------------------
-async def creatLabby(connection):
+async def create_lobby(connection):
   # 房间数据
   custom = {
-    "customGameLobby": {
-      "configuration": {
-        "gameMode": "PRACTICETOOL",
-        "gameMutator": "",
-        "gameServerRegion": "",
-        "mapId": 11, 
-        "mutators": {"id": 1}, 
-        "spectatorPolicy": "AllAllowed", 
-        "teamSize": 5
+    'customGameLobby': {
+      'configuration': {
+        'gameMode': 'PRACTICETOOL',
+        'gameMutator': '',
+        'gameServerRegion': '',
+        'mapId': 11, 
+        'mutators': {'id': 1}, 
+        'spectatorPolicy': 'AllAllowed', 
+        'teamSize': 5
       },
-      "lobbyName": "训练模式",
-      "lobbyPassword": ""
+      'lobbyName': '训练模式',
+      'lobbyPassword': ''
     },
-    "isCustom": True
+    'isCustom': True
   }
 
-  lobby = await connection.request('post', '/lol-lobby/v2/lobby', data=custom)
+  lobby = await connection.request('POST', '/lol-lobby/v2/lobby', data=custom)
 
   if lobby.status == 200:
     print(f'[创建5V5训练模式] 已成功创建 训练房间')
@@ -73,19 +63,26 @@ async def creatLabby(connection):
 #-------------------------------------------------------------------------------
 # 快速添加机器人
 #-------------------------------------------------------------------------------
-async def addBots(connection):
+async def add_team2_bots(connection):
   # 获取自定义模式电脑玩家列表
   activedata = await connection.request('GET', '/lol-lobby/v2/lobby/custom/available-bots')
   champions = { bot['name']: bot['id'] for bot in await activedata.json() }
+
+  # 队伍列表
+  team2 = ['诺克萨斯之手', '德玛西亚之力', '曙光女神', '皮城女警', '堕落天使']
+
   # 添加机器人
-  team2 = ["诺克萨斯之手", "德玛西亚之力", "曙光女神", "皮城女警", "众星之子"]
   for name in team2:
-    bots = {
-      "championId": champions[name],
-      "botDifficulty": "MEDIUM",
-      "teamId": "200"
-    }
+    bots = { 'championId': champions[name], 'botDifficulty': 'MEDIUM','teamId': '200' }
     await connection.request('post', '/lol-lobby/v1/lobby/custom/bots', data=bots)
+                                              
+
+#-------------------------------------------------------------------------------
+# 快速添加机器人
+#-------------------------------------------------------------------------------
+async def add_team1_bots(connection):
+  soraka = { 'championId': 16, 'botDifficulty': 'MEDIUM', 'teamId': '100' }
+  await connection.request('post', '/lol-lobby/v1/lobby/custom/bots', data=soraka)
 
 #-------------------------------------------------------------------------------
 # Main
@@ -93,12 +90,14 @@ async def addBots(connection):
 
 @connector.ready
 async def connect(connection):
-  await getSummonerInfo(connection)
-  await creatLabby(connection)
-  await addBots(connection)
+  await get_summoner_info(connection)
+  await create_lobby(connection)
+  await add_team2_bots(connection)
+  await add_team1_bots(connection)
 
 @connector.close
 async def disconnect(connection):
-    print('Finished')
+    print('The client was closed')
+    await connector.stop()
 
 connector.start()
